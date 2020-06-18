1035644cf37ad70118a51262009590394be5b080
Jason Howie <howiejason93@gmail.com>
2020-06-18 10:54:31 -0400

    Created Models

---
+++ 09_Capstone/Capstone/Models/Campground.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class Campground
+    {
+        public int CampgroundId { get; set; }
+        public string Name { get; set; }
+        public int OpenMonth { get; set; }
+        public int CloseMonth { get; set; }
+        public decimal DailyFee { get; set; }
+        public List<Site> Sites { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name}";
+        }
+    }
+}
---
+++ 09_Capstone/Capstone/Models/Site.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Models
+{
+    public class Site
+    {
+        public int SiteId { get; set; }
+        public bool IsAvailable { get; set; }
+        public int SiteNumber { get; set; }
+        public int MaxOccupancy { get; set; }
+        public bool IsAccessible { get; set; }
+        public int MaxRvLength { get; set; }
+        public bool HasUtilites { get; set; }
+        public List<Reservation> Reservations { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.SiteNumber}";
+        }
+    }
+}
